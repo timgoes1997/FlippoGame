d96386cec16f4cea5138fa1b595a088bc950d083
Tim Goes <Tim_Goes4@hotmail.nl>
2017-09-13 14:18:53 +0200

    Flippo basis toegevoegd

---
+++ FlippoGame/Assets/Scripts/Flippo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Flippo {
+    public int id;
+    public int amount;
+    public bool unlocked;
+    public Image flippoImage;
+    public UnityEvent questionEvent; //For loading a certain scene or something else.
+}
