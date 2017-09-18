5b69a8a1463e70a85905856fb514ce113896a406
Tim Goes <Tim_Goes4@hotmail.nl>
2017-09-18 12:18:36 +0200

    Made the Collection menu and a partial implementation of the gamemanger

---
+++ FlippoGame/Assets/Scripts/CollectionFlippo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionFlippo : MonoBehaviour {
+
+    [SerializeField]
+    private Image myIcon;
+
+    [SerializeField]
+    public GameObject gridButton;
+
+    private Flippo flippo;
+
+	// Use this for initialization
+	void Start () {
+        myIcon = gridButton.GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetFlippoItem(Flippo flippo)
+    {
+        if(myIcon == null)
+        {
+            myIcon = gridButton.GetComponent<Image>();
+        }
+
+        myIcon.sprite = flippo.sprite;
+        this.flippo = flippo;
+    }
+
+    public void OnClick()
+    {
+        if(flippo != null)
+            Debug.Log("Pressed flippo:" + flippo.id);
+    }
+}
