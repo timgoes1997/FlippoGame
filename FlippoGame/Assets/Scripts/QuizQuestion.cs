597aa411f7268c5a7bf5dc10a43095a939a53d0b
CoenWVDO <vwehv@hotmail.com>
2017-09-19 13:36:06 +0200

    QuizManager and QuizQuestion added

---
+++ FlippoGame/Assets/Scripts/QuizQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizQuestion {
+    public string questionText;
+    public string answerAText;
+    public string answerBText;
+    public string answerCText;
+    public string answerText;
+    public string imageName;
+    public bool completed = false;
+}
